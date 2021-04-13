59bb3cfde96e4f04a173df5209c147fa80f53a5f
Sjors Wijsman <sjorswijsman@gmail.com>
2021-04-13 13:15:48 +0200

    Added simple youtube search

--- index.js
+++ index.js
@@ -3,10 +3,21 @@ const app = express()
 const http = require('http').createServer(app)
 const path = require('path')
 const io = require('socket.io')(http)
-const { searchVideos } = require('./modules/searchVideos')
+const { getVideos } = require('./modules/searchVideos')
 const port = process.env.PORT || 4000
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.resolve('public')))
+
+app.set('view engine', 'ejs');
+
+app.get('/', async (req, res) => {
+  res.render('index', {
+    results: await getVideos(req.query.searchQuery) || [],
+    searchQuery: req.query.searchQuery
+  })
+})
 
 io.on('connection', (socket) => {
   console.log('user connected')
@@ -19,5 +30,3 @@ io.on('connection', (socket) => {
 http.listen(port, () => {
   console.log(`listening to port ${port}`)
 })
-
-console.log(searchVideos('masayoshi takanaka'))
--- modules/searchVideos.js
+++ modules/searchVideos.js
@@ -1,15 +1,44 @@
-const youtubeSearch = require('youtube-search')
+const axios = require('axios')
+const querystring = require('querystring')
+
 require('dotenv').config()
 
-const options = {
-  maxResults: 10,
+const params = {
+  part: 'snippet',
+  maxResults: 3,
   key: process.env.API_KEY,
+  type: 'video',
 }
 
-exports.searchVideos = (query) => {
-  youtubeSearch(query, options, (err, results) => {
-    if(err) return console.log(err)
-  
-    console.dir(results)
+function searchVideos(term) {
+  return axios.get('https://www.googleapis.com/youtube/v3/search?' + querystring.stringify({
+    ...params,
+    q: term,
+  }))
+  .then(response => {
+    const result = response.data
+    const videos = result.items.map(video => {
+      return {
+        id: video.id.videoId,
+        link: 'https://www.youtube.com/watch?v=' + video.id.videoId,
+        publishedAt: video.snippet.publishedAt,
+        channelId: video.snippet.channelId,
+        channelTitle: video.snippet.channelTitle,
+        title: video.snippet.title,
+        description: video.snippet.description,
+        thumbnails: video.snippet.thumbnails
+      }
+    })
+    return videos
   })
+  .catch(err => {
+    console.log(err)
+  })
+}
+
+
+exports.getVideos = async (term) => {
+  if (term) {
+    return await searchVideos(term)
+  }
 }
